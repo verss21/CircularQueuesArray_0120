e28bd777940217b3421ed3f54b45f0ff35a50dd6
verss21 <arjunafrizal01@gmail.com>
2024-05-31 10:34:06 +0700

    else

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -28,4 +28,13 @@ public:
 				FRONT = 0;
 				REAR = 0;
 			}
+			else {
+				//jika rear berada di posisi terakhir array, kembali ke awal array
+				if (REAR == max - 1)
+					REAR = 0;
+				else
+					REAR = REAR + 1;
+			}
+			queue_array[REAR] = num;
+	}
 
