0e58554b299fca3313b9a3cdec30b7849700b298
verss21 <arjunafrizal01@gmail.com>
2024-05-31 10:59:44 +0700

    front position

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -62,10 +62,31 @@ public:
 
 	void display() {
 		int FRONT_position = FRONT;
-		int rear_position = REAR;
+		int REAR_position = REAR;
 
 		//cek apakah antrian kosong
 		if (FRONT == -1) {
 			cout << "Queue is empty\n";
 			return;
 		}
+
+		cout << "\nElement in the queue are...\n";
+
+		// jika FRONT_position <=  REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "	";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REAR position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position;
+			}
+
+			FRONT_position = 0;
+
+			// iterasi
