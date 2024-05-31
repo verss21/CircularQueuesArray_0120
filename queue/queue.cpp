079056316513d7aebb91a6e45b504d0927cdaca7
verss21 <arjunafrizal01@gmail.com>
2024-05-31 10:45:12 +0700

    if else

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -51,4 +51,12 @@ public:
 			FRONT = -1;
 			REAR = -1;
 		}
+		else {
+			//jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
 
