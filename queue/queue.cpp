0de45de366ef862ec95eff4802f586bc85387289
verss21 <arjunafrizal01@gmail.com>
2024-05-31 10:31:42 +0700

    cek antrian

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -19,5 +19,13 @@ public:
 
 		//cek apakah antrian penuh
 		if (FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+			}
 
+			//cek apakah antrian kosong
+			if (FRONT == -1) {
+				FRONT = 0;
+				REAR = 0;
+			}
 
