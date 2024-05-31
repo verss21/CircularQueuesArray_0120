492e26bc91728ba81981dec353880e4616ed780c
verss21 <arjunafrizal01@gmail.com>
2024-05-31 10:27:37 +0700

    cek antrian

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -11,11 +11,13 @@ public:
 		REAR = -1;
 	}
 
-	void insert(){
+	void insert() {
 		int num;
 		cout << "Enter a number: ";
 		cin >> num;
 		cout << endl;
 
 		//cek apakah antrian penuh
+		if (FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+
 
